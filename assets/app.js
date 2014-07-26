3e381eb1b5b6f8bdda6e161de7d669bd45a159c1
zeke <zeke@sikelianos.com>
2014-07-25 20:20:45 -0700

    spruce up the newsletter form, and hide the TOC for now.

--- assets/app.js
+++ assets/app.js
@@ -11,10 +11,10 @@ on('keyup', function(event){
 
 on('DOMContentLoaded', function(){
 
-  document.querySelector("#toc").addEventListener('click', function(event){
-    // event.preventDefault()
-    event.stopPropagation()
-  })
+  // document.querySelector("#toc").addEventListener('click', function(event){
+  //   // event.preventDefault()
+  //   event.stopPropagation()
+  // })
 
   document.querySelector("#toggle").addEventListener('click', function(event){
     document.body.classList.toggle("disabled")
@@ -28,21 +28,21 @@ on('DOMContentLoaded', function(){
 
 })
 
-$(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      // location.hash = $(this.hash).selector.slice(1)
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 500, 'swing', function(e) {
-          console.log($(this));
-        });
-
-        return false;
-      }
-    }
-  });
-});
+// $(function() {
+//   $('a[href*=#]:not([href=#])').click(function() {
+//     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+//       var target = $(this.hash);
+//       // location.hash = $(this.hash).selector.slice(1)
+//       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+//       if (target.length) {
+//         $('html,body').animate({
+//           scrollTop: target.offset().top
+//         }, 500, 'swing', function(e) {
+//           console.log($(this));
+//         });
+//
+//         return false;
+//       }
+//     }
+//   });
+// });
