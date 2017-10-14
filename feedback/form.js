4f1bf322e80cbfdc1c5fadd0e3920157d7382401
Zeke Sikelianos <zeke@sikelianos.com>
2017-10-14 14:44:55 -0700

    componentize form styles

--- feedback/form.js
+++ feedback/form.js
@@ -6,6 +6,7 @@ $(init)
 function init () {
   $form = document.querySelector('#feedback')
   detectLocation()
+  handleFormSubmission()
 }
 
 function detectLocation () {
@@ -14,4 +15,12 @@ function detectLocation () {
       input.value = ipinfo[input.dataset.source]
     })
   })
+}
+
+function handleFormSubmission () {
+  if (location.search.match('thanks')) {
+    document.querySelector('#feedback-label').textContent = 'Thanks. Anything else you want to tell me?'
+    document.querySelector('fieldset.name').style.display = 'none'
+    document.querySelector('fieldset.email').style.display = 'none'
+  }
 }
