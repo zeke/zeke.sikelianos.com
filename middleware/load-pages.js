81e768af3c2b19b87c7b7fb382aa2d7c3e485b82
Zeke Sikelianos <zeke@sikelianos.com>
2025-07-21 12:01:51 -0700

    Update load-pages.js

--- middleware/load-pages.js
+++ middleware/load-pages.js
@@ -5,6 +5,9 @@ module.exports = async function loadPages (req, res, next) {
   if (!pages) pages = await require('../lib/pages')()
 
   set(req, 'context.pages', pages)
+  set(req, 'context.projectPages', pages.filter(page => page.kind == 'project' && !page.noIndex))
+  set(req, 'context.talkPages', pages.filter(page => page.kind == 'talk' && !page.noIndex))
+  set(req, 'context.postPages', pages.filter(page => page.kind == 'post' && !page.noIndex))
   set(req, 'context.page', pages[req.path])
 
   return next()
